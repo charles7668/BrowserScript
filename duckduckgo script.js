63eee0a9a2223b086cfa8930349d19410bdd3fd2
charles <charles76680119@gmail.com>
2021-09-26 14:53:46 +0800

    feat : let result list like to google search engine

--- duckduckgo script.js
+++ duckduckgo script.js
@@ -20,8 +20,59 @@ GM_addStyle(`
     color: red !important;
 }
 `);
+
 (function () {
   "use strict";
 
   // Your code here...
+  var pre_result_length = 0;
+  var counter = 0;
+  var change_result_node = function () {
+    var result = document.getElementsByClassName("result results_links_deep");
+    var links = document.getElementById("links");
+    if (result.length <= pre_result_length && counter < 100) {
+      setTimeout(() => {
+        counter = counter + 1;
+        change_result_node();
+      }, 50);
+    } else {
+      counter = 0;
+      pre_result_length = result.length;
+      if (counter < 100) {
+        for (var i = 0; i < result.length; i++) {
+          var clone_result = result[i].cloneNode(true);
+          var img_parent = result[i].getElementsByClassName(
+            "result__extras__url"
+          )[0];
+          var img = img_parent.getElementsByClassName("result__icon")[0];
+          clone_result
+            .getElementsByClassName("result__extras__url")[0]
+            .replaceChild(
+              img,
+              clone_result.getElementsByClassName("result__icon")[0]
+            );
+          links.replaceChild(clone_result, result[i]);
+        }
+      }
+      add_listener_to_more_result(0);
+    }
+  };
+  var add_listener_to_more_result = function (time) {
+    var more_result_node = document.getElementsByClassName("result--more__btn");
+    //console.log(more_result_node.length);
+    if (more_result_node[0] === undefined && time < 100) {
+      setTimeout(() => {
+        add_listener_to_more_result(time + 1);
+      }, 50);
+    } else if (more_result_node[0] !== undefined) {
+      more_result_node[0].addEventListener("click", () => {
+        change_result_node();
+      });
+    }
+  };
+  document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+      change_result_node();
+    }
+  };
 })();
