3682d394c6330d9a4766d8052f2caac1e6587c8a
charles <charles76680119@gmail.com>
2024-01-28 21:17:26 +0800

    Add remove-image-lazy-load script

---
+++ remove image lazy load.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         remove image lazy load
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  remove lazyload on img div
+// @author       Charles
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @match        *://**/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  let white_lists = GM_getValue("whitelists", []);
+  let host_name = document.location.hostname;
+  GM_registerMenuCommand("add site to enable remove lazy load", function () {
+    console.log(host_name);
+    // if host name is in white list, return
+    if (white_lists.includes(host_name)) {
+      return;
+    }
+    GM_setValue("whitelists", white_lists.concat(host_name));
+  });
+  GM_registerMenuCommand(
+    "remove site to disable remove lazy load",
+    function () {
+      console.log(host_name);
+      // if host name is not in white list, return
+      if (!white_lists.includes(host_name)) {
+        return;
+      }
+      GM_setValue(
+        "whitelists",
+        white_lists.filter((e) => e !== host_name)
+      );
+    }
+  );
+  // if host name is in white list , then enable remove lazy load
+  if (white_lists.includes(host_name)) {
+    // remove loading attribute
+    let images = document.getElementsByTagName("img");
+    for (let image of images) {
+      image.removeAttribute("loading");
+    }
+    // set data-src to src
+    images.setAttribute("src", images.getAttribute("data-src"));
+  }
+})();
